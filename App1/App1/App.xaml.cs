28b178e47f0103a94f4a35e877d4458fb137013e
黄煜恒 <hyhlryf@gmail.com>
2017-03-18 21:04:10 +0800

    Update dependancy packages. WARNING: This version can't run on UWP.

--- App1/App1/App.xaml.cs
+++ App1/App1/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using BuptAssistant.Settings.Ssid;
 using BuptAssistant.Toolkit;
 using Xamarin.Forms;
 
@@ -9,11 +10,11 @@ namespace BuptAssistant
 {
     public partial class App : Application
     {
-        private static SettingsDatabase _settingItemsDatabase;
+        private static SsidTable _settingItemsDatabase;
 
-        public static SettingsDatabase SettingItemsDatabase => _settingItemsDatabase ??
+        public static SsidTable SettingItemsDatabase => _settingItemsDatabase ??
                                                                (_settingItemsDatabase =
-                                                                   new SettingsDatabase(
+                                                                   new SsidTable(
                                                                        DependencyService.Get<IFileHelper>().GetLocalFilePath("SettingSQLite.db3")));
 
         public App()
